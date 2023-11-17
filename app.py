7d869deb21e1a7e6547665c7c4519356d7f66c9c
rolazazz <101631190+rolazazz@users.noreply.github.com>
2023-11-17 18:23:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class InferlessPythonModel:
     # Implement the Load function here for the model
 	def initialize(self):
 		self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-		self.model, _, self.preprocess = open_clip.create_model_and_transforms(MODEL_NAME, pretrained=MODEL_PRETRAINED, device=self.device)
+		self.model, _, self.preprocess = open_clip.create_model_and_transforms(MODEL_NAME, pretrained=MODEL_PRETRAINED, cache_dir='/var/nfs-mount/openclip-cache', device=self.device)
 		self.tokenizer = open_clip.get_tokenizer(MODEL_NAME)
 
 
